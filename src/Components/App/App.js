2db0e3306cfe9c6ac78245554161d655cc585053
Renee Swick-Ziller <reneeswick@gmail.com>
2021-12-02 17:20:03 -0700

    Render a map with a pin on San Diego Med Waste

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import './App.css';
 import DriverMap from '../DriverMap/DriverMap';
 import Welcome from '../Welcome/Welcome';
 
@@ -9,7 +11,17 @@ const App = () => {
       <h1>Routes</h1>
       <div className="content-container">
         <Welcome />
-        <DriverMap />
+        <MapContainer className="map" center={[32.886520, -117.152680]} zoom={13} scrollWheelZoom={true}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[32.886520, -117.152680]}>
+            <Popup>
+              San Diego Medical Waste
+            </Popup>
+          </Marker>
+        </MapContainer>
       </div>
     </main>
   )
