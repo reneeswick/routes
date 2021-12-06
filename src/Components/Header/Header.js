334340e5f22f03a3635b221064be491464589398
Nate Sheridan <83609521+natesheridan@users.noreply.github.com>
2021-12-05 21:52:49 -0700

    Refactor/style header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import './Header.css'
+import logo from './logoroutes.png'
 
 const Header = () => {
     return (
-        <div>
-          <h1>Routes</h1>
-          <nav>
-            <NavLink to= '/'> Home </NavLink>
-            <NavLink to = '/driver' > Driver </NavLink>
-            <NavLink to = '/customer'> Customer </NavLink>
-          </nav>
+        <div className="header">
+            <div className="nav">
+                <input type="checkbox" id="nav-check"></input>
+                    <div className="nav-header">
+                        <img className="nav-logo" src={logo} alt='logo'></img>
+                    </div>
+                <div className="nav-btn">
+                    <label htmlFor="nav-check">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </label>
+                </div>
+                <div className="nav-links">
+                    <NavLink to="/home">Home</NavLink>
+                    <NavLink to="/driver">Driver</NavLink>
+                    <NavLink to="/customer">Customer</NavLink>
+                </div>
+            </div>
         </div>
     )
 }
