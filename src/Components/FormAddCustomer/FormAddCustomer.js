c4d2a00536875b9e13489a06af1ea41c71a38190
Renee Swick-Ziller <reneeswick@gmail.com>
2021-12-09 19:08:04 -0700

    Create controlled form for creating a new customer

--- src/Components/FormAddCustomer/FormAddCustomer.js
+++ src/Components/FormAddCustomer/FormAddCustomer.js
@@ -1,10 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const FormAddCustomer = () => {
+  const [customerName, setCustomerName] = useState('');
+  const [street, setStreet] = useState('');
+  const [city, setCity] = useState('');
+  const [state, setState] = useState('')
+  const [pickupDay, setPickupDay] = useState('');
+
+  /*DriverID is defaulted to 1 -- needs to be dynamically passed as a prop*/
+  let driverID = 1
+
+  const createNewCustomer = (driverID) => {
+    let newCustomer = {
+      name: customerName,
+      driver_id: driverID
+    }
+  }
+
+  const createNewLocation = (customerID) => {
+    let newLocation = {
+      customer_id: customerID,
+      streetAdress: street,
+      city: city,
+      state: state,
+      pickupDay: pickupDay
+    }
+  }
+
+  const submitNewCustomer = () => {
+    createNewCustomer(driverID)
+    //let customerID = fetch customerID //
+    createNewLocation(/*customerID*/)
+  }
 
 
   return (
-    <h1>Form here!</h1>
+    <div>
+      <form>
+        <input
+          type='text'
+          name='customerName'
+          placeholder='Customer name here'
+          value={customerName}
+          onChange={(e) => setCustomerName(e.target.value)}
+        />
+        <input
+          type='text'
+          name='street'
+          placeholder='Street Address'
+          value={street}
+          onChange={(e) => setStreet(e.target.value)}
+        />
+        <select name='city' onChange={(e) => setCity(e.target.value)}>
+          <option value='none' selected disabled hidden>Select a City</option>
+          <option value='San Diego'>San Diego</option>
+        </select>
+        <select name='state' onChange={(e) => setState(e.target.value)}>
+          <option value='none' selected disabled hidden>Select a State</option>
+          <option value='CA'>CA</option>
+        </select>
+        <select name='day' onChange={(e) => setPickupDay(e.target.value)}>
+          <option value='none' selected disabled hidden>Select a Pick-Up Day</option>
+          <option value='Sunday'>Sunday</option>
+          <option value='Monday'>Monday</option>
+          <option value='Tuesday'>Tuesday</option>
+          <option value='Wednesday'>Wednesday</option>
+          <option value='Thursday'>Thursday</option>
+          <option value='Friday'>Friday</option>
+          <option value='Saturday'>Saturday</option>
+        </select>
+      </form>
+      <button onClick={()=>{submitNewCustomer()}}>Create New Customer</button>
+    </div>
   )
 }
 
