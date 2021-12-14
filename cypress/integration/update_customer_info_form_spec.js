c0343122de5e9e1e37a5800913291cd441c7abc3
Renee Swick-Ziller <reneeswick@gmail.com>
2021-12-14 13:30:03 -0700

    Add passing tests for updating customer info form

---
+++ cypress/integration/update_customer_info_form_spec.js
@@ -0,0 +1,23 @@
+describe('Update customer info form flow', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/update-customer-account')
+  });
+
+  it('Should see a subheader reflecting the new view', () => {
+    cy.get('.subheader').should('contain', 'Update')
+  });
+
+  it('Should be able to select a new pickup day', () => {
+    cy.get('select[name="customerDay"]')
+      .select('Monday')
+  });
+
+  it('Should be able to select a new amount of bins for service', () => {
+    cy.get('select[name="customerBins"]')
+      .select('4')
+  });
+
+  it.skip('Should be able to submit updated customer info', () => {
+    cy.get('customer-form-btn').click()
+  });
+});
